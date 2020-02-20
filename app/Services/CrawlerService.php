f8be11f576301d148448060bc96c5e6b3cd06fc8
Murilo Rocha <murilorr90@gmail.com>
2020-02-20 18:27:35 -0300

    first commit

---
+++ app/Services/CrawlerService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+
+class CrawlerService
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
